aa87392015600f2db7543f98ebce3c532e2b4914
adjit <ajtawil@gmail.com>
2018-07-07 21:57:42 -0400

    Updated readme and commented server.js
    updated the readme to contain application information, and added comments to server.js explaining the servers responsibilities

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# NodeApp
+# NodeApp for Assignment 5 CS 612
+Aaron Tawil
+
+This repository should have the ability to just be downloaded and run in docker. The node modules required are already present in the repository.
+
+# Data
+- `garage.json` contains a "garage" object with an array of cars in the garage
+- `server.js` is the running point for the application, as stated in the `package.json`
+
+# Running
+- To run from within the `NodeApp` repository with `Docker` running run as follows
+> `docker build -t <imageName> -f .\\.Dockerfile .`
+>
+> `docker run --rm -d -p 8080:8080 <imageName>:latest`
+
+Then open to `localhost:8080` to see "Hello World"
+
+`localhost:8080/garage` will yield all cars in the garage
+
+`localhost:8080/garage/{tagID}` will yield the car in the garage at the `tagID` index. If the `tagID` requested doesn't represent a car in the garage the application will send a `404` message saying that the `tagID` is out of bounds.
--- server.js
+++ server.js
@@ -3,13 +3,16 @@
 const express = require('express');
 const fs = require('fs');
 
+//  set PORT to 8080 and HOST to Localhost
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+//  initialize the express application
 const app = express();
 
 var garageList;
 
+//  Read in the contents of the garage.json file and save it to garageList
 fs.readFile(__dirname + "/" + "garage.json", "utf8", function(err, data){
     if(err) {
         console.log("Error reading file garage.json");
@@ -19,15 +22,17 @@ fs.readFile(__dirname + "/" + "garage.json", "utf8", function(err, data){
     console.log(garageList.garage);  
 });
 
+//  For the base url of the application say Hello World
 app.get('/', (req, res) => {
-    //res.status(404).send('Sorry, we cannot find that!');
     res.send('Hello World\n');
 });
 
+//  For localhost:8080/garage yield entire garageList
 app.get('/garage', (req, res) => {
     res.send(garageList);
 });
 
+//  For localhost:8080/garage/{tagID} yield that specific car or an error
 app.get('/garage/:tagId', (req, res) => {
     var tagId = req.params['tagId'];
     if(tagId == 0 || tagId > garageList.garage.length) {
